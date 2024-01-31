7214831f9f13719f26c90d21d4ae3153565699c2
Jordan Candy <Jordan.candy07@outlook.com>
2024-01-31 11:21:57 +0200

    Almost complete logic

--- index.js
+++ index.js
@@ -1,159 +1,88 @@
-// let clickCounter = 1;
-// $(".imgDiv").hide();
-// $(".warning1").hide();
-// $(".warning").hide();
-// $(".troubleshooter").hide();
-// $(".fullProgress").hide();
-// $(".pyro").hide();
-// $(".b2").click(()=>{
-//     $(".warning").hide();
+$(".troubleshooter").hide();
+$(".warning").hide();
+$(".fullProgress").hide();
+$(".pyro").hide();
 
-//     switch(clickCounter){
-//         case 1:
-//             $(".fullProgress").show();
-//             clickCounter +=1;
-//             break;
+$(document).one("click", () => {
+    setTimeout(() => {
+        $("body").addClass("newBody");
+        $(".warning").show();
+        $(".cover").hide();
+        $(".navbar").hide();
+    }, 2000);
+});
 
-//         case 2:
-//             $(".warning").show();
-//             $(".b2").click(()=>{
-//                 $(".troubleshooter").show();
-//                 setTimeout(() =>{
-//                     $(".li3").hide();
-//                 }, 1500);
-//                 $(".warning").hide();
-//                 clickCounter +=1;
-//             });
-//             break;
-//     }
-// });
+$(".btn").click(() =>{
+    move();
+    $(".btn").hide();
+});
 
+let i = 0;
 
-// //progress bar
-// var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = $("#myBar");
+    var width = 1;
+    var id = setInterval(frame, 70);
 
-// function move() {
-//   if (i == 0) {
-//     i = 1;
-//     var elem = $("#myBar");
-//     var width = 1;
-//     var id = setInterval(frame, 70);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+        setTimeout(()=>{
+            $(".fullProgress").hide();
+            $(".warning").show();
+            $(".header").text("Virus detected!");
+            $(".info").text("Run troubleshooter?");
+        }, 2000);
+      } else {
+        width++;
+        elem.css("width", width + "%");
+      }
+    }
+  }
+}
 
-//     function frame() {
-//       if (width >= 100) {
-//         clearInterval(id);
-//         i = 0;
-//         setTimeout(()=>{
-//             $(".fullProgress").hide();
-//             $(".warning").show();
-//             $(".header").text("Virus detected!");
-//             $(".info").text("Run troubleshooter?");
-//         }, 2000);
-//       } else {
-//         width++;
-//         elem.css("width", width + "%");
-//       }
-//     }
-//   }
-// }
+let clickCounter = 0;
 
-// $(".btn").click(()=>{
-//     move();
-//     $(".btn").hide();
-// });
+$(".b2").click(() =>{
+    clickCounter += 1;
+    switch(clickCounter){
+        case 1:
+            $(".fullProgress").show();
+            $(".warning").hide();
+            break;
 
-// //     if(clickCounter === 4){
-// //         console.log(clickCounter);
-// //         $(".header").text("Will you be my victim?");
-// //         setTimeout(() =>{
-// //             $(".header").text("Will you be my valentine?");
-// //         }, 2000);
-// //         $(".warningInfo").text("Pretty please :')");
-// //         $(".info").text("I promise I'll be the best valentine ever!");
-// //         $(".warning").show();
-// //         clickCounter += 1;
+        case 2:
+            $(".warning").hide();
+            $(".troubleshooter").show();
+            $(".b5").click(() =>{
+                $(".li2").hide();
+            })
+            setTimeout(()=>{
+                $(".li3").hide();
+            }, 1500);
+            break;
 
-// //     }
-    
-// //     if(clickCounter === 5){
-// //         $(".b2").click(()=>{
-// //             $("body").addClass("fireworks");
-// //             $(".pyro").show();
-// //             $(".troubleshooter").hide();
-// //         })
-// //     }
-// // });
+        case 3:
+            $(".warning").show();
+            $(".troubleshooter").hide();
+            $(".warningInfo").text("sorry");
+            $(".header").text("One last thing");
+            $(".info").text("I swear");
+            break;
 
-// // $(".b4").click(() => {
-// //     console.log(clickCounter);
+        case 4:
+            $(".warningInfo").text("Pretty Please <3");
+            $(".header").text("Will you be my valentine?");
+            $(".info").text("I will be the best valentine ever!");
+            break;
 
-// //     if(clickCounter === 3){
-// //         $(".troubleshooter").hide();
-// //         $(".warning").show();
-// //     }
-// // });
-
-
-// $(".b4").click(() => {
-//    if(clickCounter === 3){
-//             $(".troubleshooter").hide();
-//             $("warningInfo").text("Sorry");
-//             $(".header").text("One last thing...");
-//             $(".info").text("I swear");
-//             $('.warning').show();
-//             $(".warning1").show();
-//    }
-
-// });
-
-// $(".b3").click(() =>{
-//     alert("There's no getting out of this 😈");
-// });
-
-// $(".b1").click(()=>{
-//     setTimeout(()=>{
-//         $(".warning").hide();
-//     });
-
-//     setTimeout(() =>{
-//         $(".warning").show();
-//         alert("Sike!")
-//     }, 1500);
-// });
-
-// $(".b5").click(() =>{
-//     $(".li2").hide();
-// });
-
-// $(".b7").click(()=>{
-//     clickCounter = 4;
-//             if(clickCounter === 4){
-//                 $(".header").text("Will you be my victim?");
-//                 setTimeout(() =>{
-//                     $(".header").text("Will you be my valentine?");
-//                 }, 2000);
-//                 $(".warningInfo").text("Pretty please :')");
-//                 $(".info").text("I promise I'll be the best valentine ever!");
-//                 $(".warning").show();
-//                 clickCounter += 1;
-//             }
-// });
-
-// $(".b2").click(()=>{
-//     if(clickCounter === 5){
-//         $(".pyro").show();
-//     }
-
-// });
-
-// $(document).click(() =>{
-//    setInterval(() =>{
-//     $("body").addClass("newBody");
-//     $(".cover").hide();
-//     $(".navbar").hide();
-//     $(".warning").show();
-//     $(".b2").click(() =>{
-//         $(".warning").hide();
-//     })
-//    }, 2000);
-// });
+        case 5:
+            $(".pyro").show();
+            $("body").addClass("fireworks");
+            $(".warning").hide();
+            break;
+    }
+});
